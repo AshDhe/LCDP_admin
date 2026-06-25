956ab7670510595b3788749f94a690d8f774712d
AshDhe <157318874+AshDhe@users.noreply.github.com>
2026-06-25 11:14:53 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ADMIN_CONFIG = {
   ADMIN_BASE_URL: "https://admin.lacleduparc.fr",
 
-  API_LOG_SESS_AD: "https://log-sess-ad-api.lacleduparc.fr",
+  API_LOG_SESS_AD: "https://ad-log-sess-api.lacleduparc.fr",
 
   API_DASHBOARD_AD: "https://dashboard-ad-api.lacleduparc.fr",
   API_ADMIN_PARC: "https://admin-parc-api.lacleduparc.fr"
