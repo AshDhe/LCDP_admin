a165545e9a05101fd7781dd1467b1bb98f1f26f9
AshDhe <157318874+AshDhe@users.noreply.github.com>
2026-07-29 17:27:18 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -48,8 +48,7 @@
     dashboardCapacite: "https://dashboard-capacite-api.lacleduparc.fr",
 
     autocompleteParc: "https://autocomplete-parc-api.lacleduparc.fr",
-    editingAdmin: "https://editing-admin-api.lacleduparc.fr",
-    editingAdminFallback: "https://w-editing-admin.hugues-pavret.workers.dev"
+    editingAdmin: "https://editing-admin-api.lacleduparc.fr"
 
   };
 
@@ -89,7 +88,6 @@
 
     workerAutocompleteParcUrl: WORKERS.autocompleteParc,
     workerEditingAdminUrl: WORKERS.editingAdmin,
-    workerEditingAdminFallbackUrl: WORKERS.editingAdminFallback,
 
     PUBLIC_BASE: active.publicBase,
     MEMBRE_BASE: active.membreBase,
@@ -115,7 +113,6 @@
 
     WORKER_AUTOCOMPLETE_PARC_URL: WORKERS.autocompleteParc,
     WORKER_EDITING_ADMIN_URL: WORKERS.editingAdmin,
-    WORKER_EDITING_ADMIN_FALLBACK_URL: WORKERS.editingAdminFallback,
 
     publicUrl(path) {
       return buildUrl(active.publicBase, path);
@@ -165,8 +162,7 @@
     API_DASHBOARD_CAPACITE: WORKERS.dashboardCapacite,
 
     API_AUTOCOMPLETE_PARC: WORKERS.autocompleteParc,
-    API_EDITING_ADMIN: WORKERS.editingAdmin,
-    API_EDITING_ADMIN_FALLBACK: WORKERS.editingAdminFallback
+    API_EDITING_ADMIN: WORKERS.editingAdmin
 
   };
 })();
